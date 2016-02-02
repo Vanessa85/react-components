b97f6c779d5153e08ad22960c8cbb683e247d3ac
Vanessa Milagros <vanessapr.85@gmail.com>
2016-02-02 10:14:39 -0500

    modified routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,7 @@ class Header extends React.Component {
 
   render() {
   	var pathName = this.context.router.getCurrentPathname();
+  	console.log('pathName', pathName);
 
     return (
       <header>
@@ -24,8 +25,8 @@ class Header extends React.Component {
 		    </div>
 		    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       			<ul className="nav navbar-nav">
-        			<li className={pathName == '/'? 'active': ''}><Link to="simple-table">SimpleTable</Link></li>
-        			<li className={pathName == '/input-control'? 'active': ''}><Link to="input-control">InputControl</Link></li>
+        			<li className={pathName == '/react-components'? 'active': ''}><Link to="simple-table">SimpleTable</Link></li>
+        			<li className={pathName == '/react-components/input-control'? 'active': ''}><Link to="input-control">InputControl</Link></li>
       			</ul>
       		</div>
 		  </div>
